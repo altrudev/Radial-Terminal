125c1e7decf2d805cbc5af41c8c0a835c24f3c9d
Altru.dev <altrudevelop@gmail.com>
2026-09-14 19:09:25 -0700

    build: configure Kotlin JVM toolchain

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "2.4.20" apply false
+}
+
+allprojects {
+    group = "dev.altru.radialterminal"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
