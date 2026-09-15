e67764105f771d7cb7b29ef7e7cbce0f633b0274
Altru.dev <altrudevelop@gmail.com>
2026-09-14 19:55:25 -0700

    build: add Android application module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "radial-terminal"
 
+include(":app")
 include(":core-assurance")
