2c7aa147102549e658dfd48d655c9fa6f0e34078
Altru.dev <altrudevelop@gmail.com>
2026-09-14 19:57:45 -0700

    build: align Android Kotlin setup with AGP 9.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "2.4.20" apply false
     id("com.android.application") version "9.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }
 
