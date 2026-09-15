0feaf1979c0356f9e5eccb0466d58908b8109afb
Altru.dev <altrudevelop@gmail.com>
2026-09-14 19:55:28 -0700

    build: add Android and Compose plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     kotlin("jvm") version "2.4.20" apply false
+    id("com.android.application") version "9.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }
 
 allprojects {
     group = "dev.altru.radialterminal"
-    version = "0.1.0-SNAPSHOT"
+    version = "0.2.0-SNAPSHOT"
 
     repositories {
+        google()
         mavenCentral()
     }
 }
